13120aa4eca5b1d2ac9fc800215739c1adb388b4
charan <89964531+mrcharan21@users.noreply.github.com>
2026-02-06 13:57:42 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth-guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'login' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },                         
+  { path: 'login', component: LoginComponent },                // Public route
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },               // Protected route
+  { path: '**', redirectTo: 'login' }                 // Wildcard route 
 ];
